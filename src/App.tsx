e0edae49625b9149334ef783d62d5a19f46ee979
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2022-07-14 20:25:35 -0400

    Create basic chat app using socket.io
    The app will connect to the Express server hosted at
    http://localhost:5000 and connect to socket.io. Then you can send
    messages over the connection to other clients who are connected.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useContext, useCallback, useEffect } from 'react';
+import { SocketContext, socket } from "./context/socket";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SocketContext.Provider value={socket}>
+      <MessageList />
+      <ChatForm />
+    </SocketContext.Provider>
+  );
+}
+
+function MessageList() {
+
+  const socket = useContext(SocketContext);
+  const [messageList, setMessageList] = useState<string[]>([]);
+  const addMessageToList = useCallback((message: string) => {
+    setMessageList((messageList: string[]) => messageList.concat(message));
+  }, []);
+  useEffect(() => {
+    console.log('useEffect');
+    socket.on("sendMessage", addMessageToList);
+  }, [addMessageToList, socket]);
+  return (
+    <React.Fragment>
+      {messageList.map((message, i) => <p key={i}>{message}</p>)}
+    </React.Fragment>
+  );
+}
+
+function ChatForm() {
+  
+  const socket = useContext(SocketContext);
+  const [formValue, setFormValue] = useState('');
+  const sendMessage = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    socket.emit("sendMessage", formValue);
+    setFormValue('');
+  }, [formValue, socket]);
+  const onInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setFormValue(e.target.value),
+    []);
+
+  return (
+    <form onSubmit={sendMessage}>
+      <input value={formValue}
+        placeholder="Write something..."
+        onChange={onInputChange} />
+      <button type="submit">Send</button>
+    </form>
   );
 }
 
