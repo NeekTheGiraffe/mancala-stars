94d41bcf060a5f6f40a44db8d23174f5035372cf
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2022-08-12 10:55:24 -0400

    Point app to Heroku instead of localhost

--- src/context/socket.ts
+++ src/context/socket.ts
@@ -2,7 +2,8 @@ import React from 'react';
 import socketio, { Socket } from "socket.io-client";
 import { ServerToClientEvents, ClientToServerEvents } from '../types/socketTypes';
 
-const SOCKET_URL = "http://localhost:5000";
+//const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "https://mancala-stars.herokuapp.com";
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = socketio(SOCKET_URL);
 export const SocketContext = React.createContext(socket);
